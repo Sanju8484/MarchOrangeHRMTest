d384098c0b12f41fbfad17b972a5aba99d92e40d
Admin <Admin@Sanjay>
2022-05-06 12:37:37 +1400

    It is our First commit

---
+++ src/main/java/com/BaseLayer/BaseClass.java
@@ -0,0 +1,40 @@
+package com.BaseLayer;
+
+import java.io.FileInputStream;
+import java.time.Duration;
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BaseClass {
+	public static WebDriver driver;
+	public static Properties prop;
+
+	public BaseClass() {
+		prop = new Properties();
+		try {
+			FileInputStream fis = new FileInputStream(
+					System.getProperty("user.dir") + "\\com.Config\\Config.properties");
+			prop.load(fis);
+		} catch (Exception e) {
+
+			e.printStackTrace();
+		}
+
+	}
+
+	public static void iniliatization() {
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user dir") + "Drivers\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().timeouts()
+				.implicitlyWait(Duration.ofSeconds(Integer.parseInt(prop.getProperty("implicitwait"))));
+		driver.manage().timeouts()
+				.pageLoadTimeout(Duration.ofSeconds(Integer.parseInt(prop.getProperty("pageLoadtimeout"))));
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.get(prop.getProperty("url"));
+
+	}
+
+}
